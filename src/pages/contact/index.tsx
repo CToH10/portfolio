e21e3f924730df3bdacffca0f8f53b554c38e946
Luís Nunes <luis.ebn98@gmail.com>
2023-03-10 10:49:00 -0300

    feat: add contactInfo

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -1,5 +1,10 @@
 import { BaseLayout } from "../../components/BaseLayout";
+import { ContactInfo } from "../../components/Contacts";
 
 export const ContactPage = () => {
-  return <BaseLayout />;
+  return (
+    <BaseLayout>
+      <ContactInfo />
+    </BaseLayout>
+  );
 };
